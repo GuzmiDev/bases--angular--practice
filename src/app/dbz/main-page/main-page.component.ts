f91b58cb784dcaaffec4b927796d0ea1b964c72c
Erick Guzman <guzmi.dev@gmail.com>
2022-05-01 00:19:58 -0600

    :zap: @input @Outputs y EventEmitter

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500,
+    },
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 13000,
+    nombre: 'Maestro Roshi',
+    poder: 1000,
   };
 
-  agregar(): void {
-    console.log(this.nuevo);
+  agregarNuevoPersonaje(personaje: Personaje): void {
+    this.personajes.push(personaje);
   }
 }
