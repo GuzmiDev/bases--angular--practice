cd3b41430716e2a09f82b0528772ca1a517d3c84
Erick Guzman <guzmi.dev@gmail.com>
2022-04-30 21:22:31 -0600

    :art: contador Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-  base: number = 5;
-
-  acumular(valor: number) {
-    this.numero += valor;
-  }
-}
+export class AppComponent {}
